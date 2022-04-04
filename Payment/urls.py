9513d80f3540488698266abaca156071233e48be
shubham <shubham216633@gmail.com>
2022-04-04 11:24:05 +0530

    first commit

--- Payment/urls.py
+++ Payment/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path,include
 from rest_framework import routers
 from .import views
-# from .models import Domain
 
 routers=routers.DefaultRouter()
 routers.register(r'Complaint',views.ComplaintViewset)
